7e1fc0dd4fc9115f2ec5d25fcef68509b7a1651f
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-11 12:58:47 +0000

    updated again for vercel testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">CV Builder Mo Fucker</h1>
+      <h1 className="header__title">CV Builder Mo Fucker Hoe</h1>
     </header>
   )
 };   
